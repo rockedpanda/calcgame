ff4d7a5f47972674b939f87ceedae3b153fb1fc3
RockedPanda <chenghuawei@gmail.com>
2022-04-07 09:55:52 +0800

    增加声音提醒

--- js/audio.js
+++ js/audio.js
@@ -1,9 +1,31 @@
-const audio = {
+window.audio = {
   cache:{},
-  init:function(){
-
+  types:['correct','wrong','good'],
+  init:function(dom){
+    console.log(dom);
+    let fix = dom.canPlayType('audio/ogg') =='probably'?'.ogg':'.mp3';
+    this.types.forEach((x)=>{
+      fetch('./audio/'+x+fix).then(x=>x.blob()).then(t=>{
+        this.cache[x] = t;
+      });
+    });
   },
-  play:function(type){
-
+  play:function(dom, type){
+    dom.src = URL.createObjectURL(this.cache[type]);
+    dom.play();
   }
 };
+
+window.result = {
+  data:[],
+  add:function(str, time, isCorrect){
+    this.data.push([str,time,Date.now(),isCorrect?1:0].join(','));
+    localStorage.setItem('results',this.data.join(';'));
+    /* this.data.push({
+      // start: time,
+      // end: Date.now(),
+      // str: str,
+      // isCorrect: isCorrect
+    }); */
+  }
+}
